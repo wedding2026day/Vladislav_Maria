8780345687f587a8c923f3618fd433a0df319092
wedding2026day <marina.plyasheva@mail.ru>
2026-05-07 14:02:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,44 @@ function startMusic() {
 
 document.addEventListener('click', startMusic);
 document.addEventListener('touchstart', startMusic);
+
+// ОТПРАВКА ФОРМЫ
+
+const form = document.getElementById('block6-form');
+
+form.addEventListener('submit', async function(e) {
+
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  formData.append('access_key', '7359a091-40ce-40ff-b634-9316d4ca917f');
+  formData.append('subject', 'Новая анкета со свадебного сайта');
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  const response = await fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: json
+  });
+
+  const result = await response.json();
+
+  if (result.success) {
+
+    alert('Анкета отправлена ❤️');
+
+    form.reset();
+
+  } else {
+
+    alert('Ошибка отправки');
+
+  }
+
+});
